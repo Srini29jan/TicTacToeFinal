55831bb8d9cf9c9d926fa2143f7601adb6793a26
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-05 23:52:28 +0530

    extracted method isTopLeftToBottomRightDiagonalFilledByPlayerO

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -35,13 +35,17 @@ public class Game {
             winner = Player.PLAYER_X.getValue();
         }
 
-        if (getPlayerAt(POSITION_ZERO, POSITION_ZERO) == Player.PLAYER_O.getValue()
-                && getPlayerAt(POSITION_ONE, POSITION_ONE) == Player.PLAYER_O.getValue()
-                && getPlayerAt(POSITION_TWO, POSITION_TWO) == Player.PLAYER_O.getValue()) {
+        if (isTopLeftToBottomRightDiagonalFilledByPlayerO()) {
             winner = Player.PLAYER_O.getValue();
         }
 
         return winner;
+    }
+
+    private boolean isTopLeftToBottomRightDiagonalFilledByPlayerO() {
+        return getPlayerAt(POSITION_ZERO, POSITION_ZERO) == Player.PLAYER_O.getValue()
+                && getPlayerAt(POSITION_ONE, POSITION_ONE) == Player.PLAYER_O.getValue()
+                && getPlayerAt(POSITION_TWO, POSITION_TWO) == Player.PLAYER_O.getValue();
     }
 
     private boolean isTopLeftToBottomRightDiagonalFilledByPlayerX() {
