5d2713c680829e02c2831422c020ac4db8072bf5
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-05 23:21:08 +0530

    grouped conditions which return same result

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -22,20 +22,14 @@ public class Game {
     public char getWinner() {
         char winner = '\0';
         if (isAnyRowFilledByPlayer(Player.PLAYER_X.getValue())
-                || isFirstColumnFilledByPlayer(Player.PLAYER_X.getValue())) {
+                || isFirstColumnFilledByPlayer(Player.PLAYER_X.getValue())
+                || isSecondColumnFilledByPlayer(Player.PLAYER_X.getValue())) {
             winner = Player.PLAYER_X.getValue();
         }
 
         if (isAnyRowFilledByPlayer(Player.PLAYER_O.getValue())
-                || isFirstColumnFilledByPlayer(Player.PLAYER_O.getValue())) {
-            winner = Player.PLAYER_O.getValue();
-        }
-
-        if (isSecondColumnFilledByPlayer(Player.PLAYER_X.getValue())) {
-            winner = Player.PLAYER_X.getValue();
-        }
-
-        if (isSecondColumnFilledByPlayer(Player.PLAYER_O.getValue())) {
+                || isFirstColumnFilledByPlayer(Player.PLAYER_O.getValue())
+                || isSecondColumnFilledByPlayer(Player.PLAYER_O.getValue())) {
             winner = Player.PLAYER_O.getValue();
         }
 
