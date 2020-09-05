0557ecfff3788656eabcebbc2588a53569cf2138
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-05 23:20:12 +0530

    renamed method isSecondColumnFilledByPlayerO and parameter playerO

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -31,21 +31,21 @@ public class Game {
             winner = Player.PLAYER_O.getValue();
         }
 
-        if (isSecondColumnFilledByPlayerO(Player.PLAYER_X.getValue())) {
+        if (isSecondColumnFilledByPlayer(Player.PLAYER_X.getValue())) {
             winner = Player.PLAYER_X.getValue();
         }
 
-        if (isSecondColumnFilledByPlayerO(Player.PLAYER_O.getValue())) {
+        if (isSecondColumnFilledByPlayer(Player.PLAYER_O.getValue())) {
             winner = Player.PLAYER_O.getValue();
         }
 
         return winner;
     }
 
-    private boolean isSecondColumnFilledByPlayerO(char playerO) {
-        return getPlayerAt(POSITION_ZERO, POSITION_ONE) == playerO
-                && getPlayerAt(POSITION_ONE, POSITION_ONE) == playerO
-                && getPlayerAt(POSITION_TWO, POSITION_ONE) == playerO;
+    private boolean isSecondColumnFilledByPlayer(char player) {
+        return getPlayerAt(POSITION_ZERO, POSITION_ONE) == player
+                && getPlayerAt(POSITION_ONE, POSITION_ONE) == player
+                && getPlayerAt(POSITION_TWO, POSITION_ONE) == player;
     }
 
     private boolean isFirstColumnFilledByPlayer(char player) {
