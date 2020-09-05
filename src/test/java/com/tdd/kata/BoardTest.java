09caa65bd406909991be4f0630bbdf98b444870b
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-05 21:47:07 +0530

    extracted constants and field board and initialized board before each test

--- src/test/java/com/tdd/kata/BoardTest.java
+++ src/test/java/com/tdd/kata/BoardTest.java
@@ -1,23 +1,31 @@
 package com.tdd.kata;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class BoardTest {
+
+    private static final int POSITION_ZERO = 0;
+    private Board board;
+
+    @Before
+    public void setUp() {
+        board = new Board();
+    }
+
     @Test
     public void getPlayerAtShouldReturnXAfterFirstTurn() {
-        Board board = new Board();
-        board.playAt(0, 0);
+        board.playAt(POSITION_ZERO, POSITION_ZERO);
 
-        assertThat(board.getPlayerAt(0, 0), is('X'));
+        assertThat(board.getPlayerAt(POSITION_ZERO, POSITION_ZERO), is('X'));
     }
 
     @Test
     public void getCurrentPlayerShouldReturnOAfterFirstTurn() {
-        Board board = new Board();
-        board.playAt(0, 0);
+        board.playAt(POSITION_ZERO, POSITION_ZERO);
 
         assertThat(board.getCurrentPlayer(), is('O'));
     }
