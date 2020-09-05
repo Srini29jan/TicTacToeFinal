75fe3253ca6d597ac1b9f96989b02c20f304a38d
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-05 21:26:22 +0530

    extracted constants

--- src/main/java/com/tdd/kata/Game.java
+++ src/main/java/com/tdd/kata/Game.java
@@ -3,7 +3,10 @@ package com.tdd.kata;
 public class Game {
 
     private static final int POSITION_ZERO = 0;
+    private static final int POSITION_ONE = 1;
+    private static final int POSITION_TWO = 2;
     private static final char PLAYER_X = 'X';
+    private static final char PLAYER_O = 'O';
     private final Board board;
 
     public Game() {
@@ -19,14 +22,14 @@ public class Game {
     }
 
     public char getWinner() {
-        if (getPlayerAt(POSITION_ZERO, POSITION_ZERO) == PLAYER_X && getPlayerAt(POSITION_ZERO, 1) == PLAYER_X && getPlayerAt(POSITION_ZERO, 2) == PLAYER_X) {
+        if (getPlayerAt(POSITION_ZERO, POSITION_ZERO) == PLAYER_X && getPlayerAt(POSITION_ZERO, POSITION_ONE) == PLAYER_X && getPlayerAt(POSITION_ZERO, POSITION_TWO) == PLAYER_X) {
             return PLAYER_X;
         }
 
-        if (getPlayerAt(POSITION_ZERO, POSITION_ZERO) == 'O'
-                && getPlayerAt(POSITION_ZERO, 1) == 'O'
-                && getPlayerAt(POSITION_ZERO, 2) == 'O') {
-            return 'O';
+        if (getPlayerAt(POSITION_ZERO, POSITION_ZERO) == PLAYER_O
+                && getPlayerAt(POSITION_ZERO, POSITION_ONE) == PLAYER_O
+                && getPlayerAt(POSITION_ZERO, POSITION_TWO) == PLAYER_O) {
+            return PLAYER_O;
         }
 
         return '\0';
