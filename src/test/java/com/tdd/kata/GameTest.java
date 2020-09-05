1ab63dbb312db4800874eddc2ca78b1cac5e9b8e
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-09-05 23:14:57 +0530

    checked if player X is winner if X fills second column

--- src/test/java/com/tdd/kata/GameTest.java
+++ src/test/java/com/tdd/kata/GameTest.java
@@ -139,4 +139,15 @@ public class GameTest {
 
         assertThat(game.getWinner(), is(Player.PLAYER_O.getValue()));
     }
+
+    @Test
+    public void getWinnerShouldReturnXIfXFillsSecondColumn() {
+        game.playAt(POSITION_ZERO, POSITION_ONE);
+        game.playAt(POSITION_ZERO, POSITION_ZERO);
+        game.playAt(POSITION_ONE, POSITION_ONE);
+        game.playAt(POSITION_ZERO, POSITION_TWO);
+        game.playAt(POSITION_TWO, POSITION_ONE);
+
+        assertThat(game.getWinner(), is(Player.PLAYER_X.getValue()));
+    }
 }
